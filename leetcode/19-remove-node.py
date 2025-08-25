81a4fd83db9d6b9a88dd3e340410bde195a93c68
Kevin Thomas <kevin.jt2007@gmail.com>
2025-08-24 21:51:22 -0700

    feat: improve #19

--- leetcode/19-remove-node.py
+++ leetcode/19-remove-node.py
@@ -1,3 +1,34 @@
+# from typing import Optional
+
+
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+# class Solution:
+#     def removeNthFromEnd(
+#         self, head: Optional[ListNode], n: int
+#     ) -> Optional[ListNode]:
+#         cn = head
+
+#         history = []
+
+#         while cn.next is not None:
+#             history.append(cn)
+#             cn = cn.next
+
+#         history.append(cn)
+
+#         if len(history) < n + 1:
+#             head = head.next
+#         else:
+#             history[-n - 1].next = history[-n].next
+
+#         return head
+
+
 from typing import Optional
 
 
@@ -11,19 +42,21 @@ class Solution:
     def removeNthFromEnd(
         self, head: Optional[ListNode], n: int
     ) -> Optional[ListNode]:
-        cn = head
+        first = head
+        second = head
 
-        history = []
+        for _ in range(n):
+            second = second.next
 
-        while cn.next is not None:
-            history.append(cn)
-            cn = cn.next
+        if second is None:
+            return head.next
 
-        history.append(cn)
+        while True:
+            if second.next is None:
+                first.next = first.next.next
+                break
 
-        if len(history) < n + 1:
-            head = head.next
-        else:
-            history[-n - 1].next = history[-n].next
+            first = first.next
+            second = second.next
 
         return head
