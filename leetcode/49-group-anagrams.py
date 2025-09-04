a386c7e2f0bd0878690671227a51a31f57557fe0
Kevin Thomas <kevin.jt2007@gmail.com>
2025-09-04 10:37:25 -0400

    feat: improve #49

--- leetcode/49-group-anagrams.py
+++ leetcode/49-group-anagrams.py
@@ -1,3 +1,5 @@
+# O(n*m*log(m))
+"""
 from typing import List
 
 
@@ -11,5 +13,26 @@ class Solution:
                 lib[parsed].append(s)
             else:
                 lib[parsed] = [s]
+
+        return [x for x in lib.values()]
+"""
+
+# O(n*m)
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        lib = {}
+
+        for s in strs:
+            alphabet = [0] * 26
+            for char in s:
+                alphabet[ord(char) - 97] += 1
+            tupled = tuple(alphabet)
+            if lib.get(tupled):
+                lib[tupled].append(s)
+            else:
+                lib[tupled] = [s]
 
         return [x for x in lib.values()]
