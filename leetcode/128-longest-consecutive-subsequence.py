f258a86f0ddfd0ff2242dddbce017452fb4d2425
Kevin Thomas <kevin.jt2007@gmail.com>
2025-09-07 01:20:01 -0400

    feat: improve #128

--- leetcode/128-longest-consecutive-subsequence.py
+++ leetcode/128-longest-consecutive-subsequence.py
@@ -1,3 +1,5 @@
+# O(nlogn)
+"""
 import heapq
 from typing import List
 
@@ -22,5 +24,25 @@ class Solution:
             else:
                 current = 0
             prev = num
+
+        return longest
+"""
+
+# O(n)
+from typing import List
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        setted = set(nums)
+        longest = 0
+
+        for num in setted:
+            if num - 1 in setted:
+                continue
+            y = num
+            while y in setted:
+                y += 1
+            longest = max(longest, y - num)
 
         return longest
