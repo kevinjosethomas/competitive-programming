2c4ba906e02cfeeb92a7a3ebc35605259bb9b8c5
Kevin Thomas <kevin.jt2007@gmail.com>
2025-09-07 02:12:56 -0400

    feat: reimplement #15

--- leetcode/15-threesum.py
+++ leetcode/15-threesum.py
@@ -25,6 +25,7 @@ class Solution:
         return [list(triplet) for triplet in triplets]
 """
 
+"""
 from typing import List
 
 
@@ -63,5 +64,43 @@ class Solution:
                         left += 1
                     while left < right and nums[right] == nums[right + 1]:
                         right -= 1
+
+        return triplets
+"""
+
+# reattempt
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        triplets = []
+
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            j = i + 1
+            k = len(nums) - 1
+
+            while j < k:
+                s = nums[i] + nums[j] + nums[k]
+
+                if s > 0:
+                    k -= 1
+                elif s < 0:
+                    j += 1
+                else:
+                    triplets.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
 
         return triplets
