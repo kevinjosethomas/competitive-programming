721e6649fbae14ea3642e61240bdbfc6052966a4
Kevin Thomas <kevin.jt2007@gmail.com>
2025-09-08 13:48:26 -0400

    feat: reimplement #22

--- leetcode/22-generate-parentheses.py
+++ leetcode/22-generate-parentheses.py
@@ -1,3 +1,4 @@
+"""
 from typing import List
 
 
@@ -21,3 +22,26 @@ class Solution:
         dfs("", 0, 0)
 
         return options
+"""
+
+# reimplement
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        combinations = []
+        opened = 0
+        closed = 0
+
+        def dfs(s: str, opened: int, closed: int) -> str:
+            if opened == n and closed == n:
+                combinations.append(s)
+
+            if opened < n:
+                dfs(s + "(", opened + 1, closed)
+            if closed < opened:
+                dfs(s + ")", opened, closed + 1)
+
+        dfs("", opened, closed)
+        return combinations
